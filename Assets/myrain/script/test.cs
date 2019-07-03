44d98471c452a8d51c1e080c7208469788c9c84b
tubdub <b1017098@fun.ac.jp>
2019-07-03 18:03:24 +0900

    a
    a

--- Assets/myrain/script/test.cs
+++ Assets/myrain/script/test.cs
@@ -13,17 +13,12 @@ public class test : MonoBehaviour
     ParticleSystem ParticleObjLightning;
     private AudioSource audioSource;
     float m_MySliderValue;
-    //public AudioClip audioClip1;
-    //public AudioClip audioClip2;
-    //public AudioClip audioClip3;
-    //public AudioClip audioClip4;
-    //public AudioClip audioClip5;
+
+    public AudioClip audioClip1;
+
     private AudioSource[] sources;
     private AudioSource rain1;
-    private AudioSource rain2;
-    private AudioSource rain3;
-    private AudioSource rain4;
-    private AudioSource rain5;
+
     private AudioSource lightning;
     public int Count;
 
@@ -45,16 +40,15 @@ public class test : MonoBehaviour
         //ParticleObjFog.Stop();
         ParticleObjLightning.Stop();
 
+        //audioClip1 = gameObject.GetComponents<AudioSource>().clip;
 
         //audioSource = gameObject.GetComponent<AudioSource>();
         sources = gameObject.GetComponents<AudioSource>();
         rain1 = sources[0];
-        rain2 = sources[1];
-        rain3 = sources[2];
-        rain4 = sources[3];
-        rain5 = sources[4];
-        lightning = sources[5];
+  
+        lightning = sources[1];
 
+        //rain1.Play();
 
         m_MySliderValue = 0.5f;
 
@@ -98,14 +92,18 @@ public class test : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             Count = 1;
+            rain1.volume = 0.2f;
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
             Count = 2;
+            rain1.volume = 0.6f;
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
             Count = 3;
+            rain1.volume = 1.0f;
+
         }
         if (Input.GetKeyDown(KeyCode.F))
         {
@@ -124,73 +122,26 @@ public class test : MonoBehaviour
             ParticleObjLightning.Stop();
             mEmObjRain.rate = new ParticleSystem.MinMaxCurve(100);
             mEmObjFog.rate = new ParticleSystem.MinMaxCurve(1);
-            //rain2.Stop(); //弱い雨STOP
-           // rain3.Stop();
-            //rain4.Stop();
-            //rain5.Stop();
-            //sources[0].Play(); //弱い雨用のaudiosurceファイルを再生
-            //audioSource.clip = audioClip1;
-            rain1.Play();
+   
+
 
         }
-        else if (Count==2) //
+        else if (Count==2) //普通の雨
         {
           //  ParticleObjFog.Play();
             ParticleObjLightning.Stop();
-            mEmObjRain.rate = new ParticleSystem.MinMaxCurve(300);
-            mEmObjFog.rate = new ParticleSystem.MinMaxCurve(3);
-           // rain1.Stop(); //弱い雨STOP
-            //rain3.Stop();
-            //rain4.Stop();
-            //rain5.Stop();
-            //sources[1].Play();
-            //audioSource.clip = audioClip2;
-            rain2.Play();
-
-        }
-        else if (Count==3) //普通の雨
-        {
-          //  ParticleObjFog.Play();
-            ParticleObjLightning.Stop();
-            mEmObjRain.rate = new ParticleSystem.MinMaxCurve(800);
-            mEmObjFog.rate = new ParticleSystem.MinMaxCurve(5);
-            //rain1.Stop(); //弱い雨STOP
-            //rain2.Stop();
-            //rain4.Stop();
-            //rain5.Stop();
-            //sources[2].Play();
-            //audioSource.clip = audioClip3;
-            rain3.Play();
-
-        }
-        else if (Count==4) //
-        {
-           // ParticleObjFog.Play();
-            ParticleObjLightning.Stop();
-            mEmObjRain.rate = new ParticleSystem.MinMaxCurve(1100);
+            mEmObjRain.rate = new ParticleSystem.MinMaxCurve(1000);
             mEmObjFog.rate = new ParticleSystem.MinMaxCurve(7);
-            //rain1.Stop(); //弱い雨STOP
-           // rain2.Stop();
-            //rain3.Stop();
-            //rain5.Stop();
-            //sources[3].Play();
-            //audioSource.clip = audioClip4;
-            rain4.Play();
+      
+
 
         }
-        else if (Count==5) //すごく強い雨
+        else if (Count==3) //すごく強い雨
         {
             //ParticleObjFog.Play();
             mEmObjRain.rate = new ParticleSystem.MinMaxCurve(2000);
             mEmObjFog.rate = new ParticleSystem.MinMaxCurve(50);
-            //rain1.Stop(); //弱い雨STOP
-            //rain2.Stop();
-            //rain3.Stop();
-            //rain4.Stop();
-            //sources[3].Stop();
-            //sources[4].Play(); //強い雨用のaudiosurceファイルを再生
-            //audioSource.clip = audioClip5;
-            rain5.Play();
+      
             if (mCount >= 5.0f)
             { //← 5秒経過する度に if 成立
                 mCount = 0; // 時間計測用変数を初期化
